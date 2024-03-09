aec407027d3117fd38844ae09a8297a6ca07c821
rishiranjan786 <rishiranjan786@gmail.com>
2024-03-09 17:05:52 +0530

    new port

---
+++ src/constants/index.js
@@ -0,0 +1,101 @@
+// import { Image  } from "./../assets/images/portfolio.png";
+export const projects = [
+  {
+    // title: "Plants vs Zombies API",
+    title: "Portfolio",
+    desc: "Dynamic portfolio website built with MERN stack showcasing projects and skills, offering seamless user experience and interactive design.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      // {
+      //   name: "NodeJs",
+      //   color: "green-600",
+      // },
+      // {
+      //   name: "MongoDb",
+      //   color: "blue-600",
+      // },
+      {
+        name: "TailwindCSS",
+        color: "gray-600",
+      },
+    ],
+    img:'\portfolio.png',
+    // link: "",
+    code: "https://github.com/rishiranjan786/portfolio.git",
+  },
+  {
+    title: "TextUtils",
+    desc: "TextUtils is a versatile tool for manipulating and analyzing text data. It offers functions for tasks such as tokenization, stemming, and sentiment analysis, aiding in text preprocessing",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+    ],
+    img:"/textutils.png",
+    // link: "",
+    code: "https://github.com/rishiranjan786/TextUtils.git",
+  },
+  {
+    title: "Lost N Find Website",
+    desc: "Lost N Find: MERN platform for seamlessly reuniting lost items with their owners. Modern, intuitive, and efficient, simplifying the process of reclaiming lost possessions in your community.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "NodeJs",
+        color: "green-600",
+      },
+      {
+        name: "MongoDb",
+        color: "blue-600",
+      },
+      {
+        name: "CSS",
+        color: "yellow-600",
+      },
+    ],
+    img: "/lostnfind.png",
+    // link: "",
+    code: "https://github.com/rishiranjan786/LostNFind.git",
+  },
+  {
+    title: "NewsWeb Website",
+    desc: "NewsWeb is an online platform aggregating news articles from various sources. Users can access diverse news content, filter by category or source, and stay updated on current events conveniently, fostering informed decision-making and broadening perspectives.",
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+      {
+        name: "JavaScript",
+        color: "blue-800",
+      },
+      {
+        name: "PHP",
+        color: "red-500",
+      },
+      {
+        name: "SQL",
+        color: "yellow-600",
+      },
+    ],
+    img: "/newsweb.png",
+    // link: "",
+    // code: "https://github.com/rishiranjan786",
+    code:"https://github.com/rishiranjan786/Newsweb.git",
+  },
+];
